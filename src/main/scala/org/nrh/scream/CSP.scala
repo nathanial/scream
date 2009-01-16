b3f732da6c9dd8bef18e4ce4fd2e2c76c4492cc0
nathan <nathan@nathan-desktop.(none)>
2009-01-16 06:32:54 -0600

    Moved Problem class into its own file.

--- src/main/scala/org/nrh/scream/CSP.scala
+++ src/main/scala/org/nrh/scream/CSP.scala
@@ -5,49 +5,6 @@ import org.nrh.scream.Domain._
 import org.nrh.scream.Interval._ 
 import org.nrh.scream.Util._
 
-class Problem(solver:Solver) { 
-  val csp = new CSP
-
-  def newVar:Var = newVar("default")
-
-  def newVar(name: String):Var = newVar(name, default_domain)
-
-  def newVar(name:String,domain:Domain):Var = {
-    val nv = new Var(csp,true)
-    nv.domainStack.push(domain)
-    nv.name = name
-    csp.addVar(nv)
-    return nv
-  }
-
-  def allDiff(vars:Var*){
-    csp.addConstraint(new DifferenceConstraint(vars.toList))
-  }
-
-  def allDiff(vars:List[Var]){
-    csp.addConstraint(new DifferenceConstraint(vars))
-  }
-
-  def propogateConstraints {
-    solver.propogateConstraints(csp)
-  }
-
-  def firstSolution {
-    solver.firstSolution(csp) match {
-      case None => throw new RuntimeException("No Solution Found")
-      case Some(s) => s.assignToVars
-    }
-  }
-
-  def allSolutions:Iterator[Option[Solution]] = solver.allSolutions(csp)
-}
-
-object Problem {
-  def standard:Problem = new Problem(new BacktrackingSolver(AC3,MRV))
-  def sudoku:Problem = new Problem(new BacktrackingSolver(AC3,MRV))
-  def minConflict:Problem = new Problem(new MinConflictsSolver(AC3,50))
-}
-
 class CSP {
   private val varBuffer = new ListBuffer[Var]
   private val constraintBuffer = new ListBuffer[Constraint]
